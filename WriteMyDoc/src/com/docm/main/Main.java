22a6ac50d2bbd114c83b33099d5faff2eb13cdc9
eduardorodriguez <eduardorodriguez@eduardos-mbp.fios-router.home>
2018-08-05 11:24:33 -0400

    Test

---
+++ WriteMyDoc/src/com/docm/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.docm.main;
+
+/**
+ * @author eduardorodriguez
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
