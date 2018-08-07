fc60c2e859a4f9d0daebf8a2cd099f5a41a996c2
eduardorodriguez <eduardorodriguez@10.0.0.28>
2018-08-06 22:27:56 -0500

    Added class employee

---
+++ WriteMyDoc-Persistance/src/com/docm/model/Employee.java
@@ -0,0 +1,52 @@
+package com.docm.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the Employee database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Employee.findAll", query="SELECT e FROM Employee e")
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	@Column(name="first_name")
+	private String firstName;
+
+	@Column(name="last_name")
+	private String lastName;
+
+	public Employee() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
